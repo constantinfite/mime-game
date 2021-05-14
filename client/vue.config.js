c72a07863b5be006cf265770669970974a311c01
Constantin FIte <constantin.fite@gmail.com>
2021-05-14 11:35:14 +0200

    modify devServer vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,4 +2,7 @@ const path = require("path");
 module.exports = {
   transpileDependencies: ["vuetify"],
   outputDir: path.resolve(__dirname, "../server/public"),
+  devServer: {
+    proxy: 'http://localhost:5000/api'
+}
 };
