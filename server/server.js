f614ab498c90a8ca997856ec909337b4ea3a566e
Constantin FIte <constantin.fite@gmail.com>
2021-05-14 11:01:52 +0200

    add json server in express

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 var history = require('connect-history-api-fallback');
 const path = require('path');
+var cors = require('cors')
 const serveStatic = require('serve-static');
+var jsonServer = require('json-server');
 
 let app = express();
+
+app.use(cors())
+app.use('/api', jsonServer.router('db.json'));
 app.use(history());
 app.use(serveStatic(__dirname + "/public/"));
 
