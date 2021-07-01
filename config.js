41935e367c4c590afedd492d84a1d94366d21fbc
constantin <constantin.fite@gmail.com>
2021-07-01 10:47:25 +0200

    test

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     //mongoUri : process.env.MONGO_URI,
     mongoUri: "mongodb+srv://constan:Callofduty23@cluster0.ub0gg.mongodb.net/mime_db?retryWrites=true&w=majority",
 
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 5000,
 }
